94d6c8a88a725dda2ac597f79223673d74b33d7d
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-08-24 11:13:58 -0600

    Probando peticiones HTTP, GET y POST

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -5,12 +5,30 @@ const router = express.Router();
 // Routing
 // request: que se esta enviando al servidor
 // response: lo que el servidor responde
-router.get("/", function (req, res) {
+// router.get("/", function (req, res) {
+//   res.send("Hola Mundo en Express desde Routing");
+// });
+
+// router.post("/", function (req, res) {
+//   res.json({ mensaje: "Usando Post en el servidor" });
+// });
+
+router.get("/", (req, res) => {
   res.send("Hola Mundo en Express desde Routing");
 });
 
-router.get("/nosotros", function (req, res) {
-  res.send("Sobre Nosotros"); 
-});
+// router.post("/", (req, res) => {
+//   res.json({ mensaje: "Usando Post en el servidor" });
+// });
+
+// Otra forma de englobar varias verbos HTTP y urls
+// router
+//   .route("/")
+//   .get("/", function (req, res) {
+//     res.send("Hola Mundo en Express desde Routing");
+//   })
+//   .post("/", function (req, res) {
+//     res.json({ mensaje: "Usando Post en el servidor" });
+//   });
 
 export default router;
