ca4605ce8f9820042f6ecea6cef49a65b878e2c6
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-08-24 10:42:19 -0600

    CReando un SERVIDOR con EXPRESS

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hola Mundo');
+// Extrar la libreria de express
+const express = require('express');
+
+// Crear la app
+const app = express();
+
+// Routing
+// request: que se esta enviando al servidor
+// response: lo que el servidor responde
+app.get('/', function (req, res) {
+  res.send('Hola Mundo en Express');
+});
+
+// Definir un puerto
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
