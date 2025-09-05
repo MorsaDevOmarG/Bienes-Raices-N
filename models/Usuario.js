d612d6638d1aadd8665689b7dd58d579f992c181
Omar <omar.garcia@morsa.com.mx>
2025-09-05 17:23:26 -0600

    Añadiendo los campos necesarios al MODELO

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,7 +1,27 @@
 import { DataTypes } from 'sequelize';
 import db from '../config/db.js';
+import e from 'express';
 
 // Nombre de la tabla que se va crear (Usuarios)
 const Usuario = db.define('usuarios', {
-
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  // token: {
+  //   type: DataTypes.STRING,
+  // },  
+  token: DataTypes.STRING,
+  confirmado: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
 });
