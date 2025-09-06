d52da8460387f181bcfa4d041f2704820c0aaa9c
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-09-06 14:23:06 -0600

    Agregando el resto de validaciones

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -22,8 +22,13 @@ const registrar = async (req, res) => {
 
   // Validación
   await check("nombre").notEmpty().withMessage("El Nombre no puede ir vacío").run(req);
+  await check("email").isEmail().withMessage("El Email debe ser válido").run(req);
+  await check("password").isLength({ min: 6 }).withMessage("El Password debe ser al menos de 6 caracteres").run(req);
+  await check("repetir_password").equals(req.body.password).withMessage("Los passwords no son iguales").run(req);
 
   let resultado = validationResult(req);
+
+  // Verificar que el resultado esté vacío
 
   res.json( resultado.array() );
 
