c7d83a0d443e67ec739bc79ecfdc2532110425df
Omar <omar.garcia@morsa.com.mx>
2025-09-19 12:38:33 -0600

    Almacenando LATITUD, LONGITUD y CALLE

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -32,6 +32,17 @@
           // console.log(resultado);
           marker.bindPopup(resultado.address.LongLabel); // Mostrar la dirección en un popup
           marker.openPopup(); // Abrir el popup automáticamente
+
+          // Llenar los campos
+          document.querySelector(".calle").textContent = resultado?.address?.Address ?? '';
+          document.querySelector("#calle").value = resultado?.address?.Address ?? '';
+          document.querySelector(".ciudad").textContent = resultado?.address?.City ?? '';
+          document.querySelector(".estado").textContent = resultado?.address?.Region ?? '';
+          document.querySelector(".pais").textContent = resultado?.address?.CountryCodeISO3 ?? '';
+          // document.querySelector(".lat").textContent = posicion.lat.toFixed(6) ?? '';
+          // document.querySelector(".lng").textContent = posicion.lng.toFixed(6) ?? '';
+          document.querySelector("#lat").value = resultado?.latlng?.lat ?? '';
+          document.querySelector("#lng").value = resultado?.latlng?.lng ?? '';
         });
   });
 
