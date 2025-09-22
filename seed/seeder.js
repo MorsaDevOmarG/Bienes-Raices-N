f63402ff74cf3de1636e2b53082e5d8d5eb77924
Omar <omar.garcia@morsa.com.mx>
2025-09-22 13:27:13 -0600

    Eliminar datos con el SEDDER

--- seed/seeder.js
+++ seed/seeder.js
@@ -4,6 +4,7 @@ import Categoria from "../models/Categoria.js";
 import precios from "./precios.js";
 import Precio from "../models/Precio.js";
 import db from "../config/db.js";
+import { truncate } from "node:fs";
 
 const importarDatos = async () => {
   try {
@@ -33,7 +34,33 @@ const importarDatos = async () => {
   }
 };
 
+const eliminarDatos = async () => {
+  try {
+    // Eliminar los datos
+    // await Promise.all([
+    //   Categoria.destroy({ where: {}, truncate: true }),
+    //   Precio.destroy({ where: {}, truncate: true }),
+    // ]);
+
+    // Otra forma de eliminar los datos
+    await db.sync({ force: true });
+
+    console.log("Datos eliminados correctamente");
+
+    exit(0); // Forma de salir de un proceso sin error
+  } catch (error) {
+    console.log(error);
+    // process.exit(1); // Forma de salir de un proceso con error
+    exit(1);
+  }
+};
+
 // Ejecutar sedder
 if (process.argv[2] === "-i") {
   importarDatos();
+}
+
+// Ejecutar eliminar
+if (process.argv[2] === "-e") {
+  eliminarDatos();
 }
