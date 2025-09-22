038e7d6cb12ac1e7db059835f74389fcbc0d3ae2
Omar <omar.garcia@morsa.com.mx>
2025-09-22 17:09:15 -0600

    Importando CATEGORIAS y PRECIOS para pasarlos a las VISTAS de CREAR

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,3 +1,6 @@
+import Categoria from "../models/Categoria.js";
+import Precio from "../models/Precio.js";
+
 const admin = (req, res) => {
   // res.send('Mis propiedades');
   res.render('propiedades/admin', {
@@ -7,11 +10,19 @@ const admin = (req, res) => {
 };
 
 // Función para crear una propiedad
-const crear = (req, res) => {
+const crear = async (req, res) => {
+  // Consultar Modelo de Precio y Categoría, es importante tomar en cuenta el orden
+  const [categorias, precios] = await Promise.all([
+    Categoria.findAll(),
+    Precio.findAll()
+  ]);
+
   // res.send('Crear propiedad');
   res.render('propiedades/crear', {
     pagina: 'Crear propiedad',
-    barra: true
+    barra: true,
+    categorias,
+    precios
   });
 };
 
