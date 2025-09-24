095c94595f12d7cb935b0b20edcfac6510937bee
Omar <omar.garcia@morsa.com.mx>
2025-09-24 13:34:39 -0600

    Añadiendo CSRF y validacion al FORM

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,3 +1,5 @@
+import { validationResult } from "express-validator";
+
 import Categoria from "../models/Categoria.js";
 import Precio from "../models/Precio.js";
 
@@ -18,12 +20,37 @@ const crear = async (req, res) => {
   ]);
 
   // res.send('Crear propiedad');
-  res.render('propiedades/crear', {
-    pagina: 'Crear propiedad',
+  res.render("propiedades/crear", {
+    pagina: "Crear propiedad",
     barra: true,
+    csrfToken: req.csrfToken(),
     categorias,
-    precios
+    precios,
   });
 };
 
-export { admin, crear };
+const guardar = async (req, res) => {
+  // Validación
+  let resultado = validationResult(req);
+  if (!resultado.isEmpty()) {
+    // Hay errores
+    // console.log(resultado.array());
+
+    // Consultar Modelo de Precio y Categoría, es importante tomar en cuenta el orden
+    const [categorias, precios] = await Promise.all([
+      Categoria.findAll(),
+      Precio.findAll()
+    ]);
+
+    return res.render("propiedades/crear", {
+      pagina: "Crear propiedad",
+      barra: true,
+      csrfToken: req.csrfToken(),
+      categorias,
+      precios,
+      errores: resultado.array(),
+    });
+  }
+};
+
+export { admin, crear, guardar };
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import { body } from 'express-validator';
 
-import { admin, crear } from '../controllers/propiedadController.js';
+import { admin, crear, guardar } from '../controllers/propiedadController.js';
 
 const router = express.Router();
 
 router.get('/mis-propiedades', admin);
 router.get('/propiedades/crear', crear);
+router.post('/propiedades/crear',
+  body('titulo').notEmpty().withMessage('El título es obligatorio'),
+  body('descripcion').notEmpty().withMessage('La descripción es obligatoria').isLength({ max: 200 }).withMessage('La descripción no puede ser mayor a 200 caracteres'),
+  body('categoria').notEmpty().withMessage('La categoría es obligatoria'),
+  guardar
+);
 
 export default router;
