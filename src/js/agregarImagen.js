5e596493490233dc958ca0d3064bf73d33968f97
Omar <omar.garcia@morsa.com.mx>
2025-10-03 17:22:29 -0600

    Configurando MULTER, correcion para subir IMAGEN

--- src/js/agregarImagen.js
+++ src/js/agregarImagen.js
@@ -9,7 +9,7 @@ Dropzone.options.imagen = {
   maxFilesize: 5, // MB
   maxFiles: 1,
   parallelUploads: 1, // Subir 1 archivo a la vez
-  autoProcessQueue: false, // Procesar la cola automáticamente
+  autoProcessQueue: true, // Procesar la cola automáticamente
   addRemoveLinks: true, // Permitir eliminar archivos
   dictRemoveFile: 'Eliminar Archivo',
   dictMaxFilesExceeded: 'Solo puedes subir 1 archivo',
