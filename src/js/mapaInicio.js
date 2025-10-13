b6ddeb547e1e668f8c1ad53e095e4867b0db3b67
Omar <omar.garcia@morsa.com.mx>
2025-10-13 13:13:53 -0600

    Mostrando los PINES de las PROPIEDADES

--- src/js/mapaInicio.js
+++ src/js/mapaInicio.js
@@ -3,6 +3,9 @@
   const lng = -103.38739216304566;
   const mapa = L.map("mapa-inicio").setView([lat, lng], 16);
 
+  // Son una capa que estará sobre el mapa
+  let markers = new L.FeatureGroup().addTo(mapa);
+
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -13,10 +16,27 @@
       const url = "/api/propiedades";
       const respuesta = await fetch(url);
       const propiedades = await respuesta.json();
-      console.log(propiedades);
+      // console.log(propiedades);
+
+      mostrarPropiedades(propiedades);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const mostrarPropiedades = (propiedades) => {
+    // console.log(propiedades);
+
+    propiedades.forEach((propiedad) => {
+      // Agregar el PIN
+      const marker = new L.marker([propiedad?.lat, propiedad?.lng], {
+        autoPan: true,
+      })
+        .addTo(mapa)
+        .bindPopup("Información aquí");
+      
+      markers.addLayer(marker);
+    });
   };
 
   obtenerPropiedades();
