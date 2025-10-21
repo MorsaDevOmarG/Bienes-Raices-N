36aad4d3951193356cc5088d29c03e00d3e61db2
Omar <omar.garcia@morsa.com.mx>
2025-10-21 16:59:27 -0600

    Creando el MODELO de MENSAJE

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ import Propiedad from './Propiedad.js';
 import Precio from './Precio.js';
 import Categoria from './Categoria.js';
 import Usuario from './Usuario.js';
+import Mensaje from './Mensaje.js';
 
 // Relaciones, una Propiedad pertene a un Precio
 // Precio.hasOne(Propiedad);
@@ -15,4 +16,7 @@ Propiedad.belongsTo(Categoria, { foreignKey: 'categoriaId' });
 // Una Propiedad pertenece a un Usuario
 Propiedad.belongsTo(Usuario, { foreignKey: 'usuarioId' });
 
-export { Propiedad, Precio, Categoria, Usuario };
+Mensaje.belongsTo(Propiedad, { foreignKey: 'propiedadId' });
+Mensaje.belongsTo(Usuario, { foreignKey: 'usuarioId' });
+
+export { Propiedad, Precio, Categoria, Usuario, Mensaje };
