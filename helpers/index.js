526f91211c26e6d63dbdaee4dd8a2d5cbdff1e62
Omar <omar.garcia@morsa.com.mx>
2025-10-23 13:55:20 -0600

    Formateando la FECHA

--- helpers/index.js
+++ helpers/index.js
@@ -3,7 +3,21 @@ const esVendedor = (usuarioId, propiedadUsuarioId) => {
 };
 
 const formatearFecha = fecha => {
-  console.log(fecha);
+  // console.log(fecha);
+  // console.log(new Date(fecha).toISOString());
+  // console.log(new Date(fecha).toISOString().split('T'));
+  // console.log(new Date(fecha).toISOString().slice(0, 10));
+
+  const nuevaFecha = new Date(fecha).toISOString().slice(0, 10);
+
+  const opciones = {
+    weekday: 'long',
+    yer: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  return new Date(nuevaFecha).toLocaleDateString('es-ES', opciones);
 };
 
 export { esVendedor, formatearFecha };
